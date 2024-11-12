8d7bf8cc9226570857717f3ff90c14283f3dd884
Luana <luana.smatos01@gmail.com>
2024-11-11 22:17:25 -0300

    Primeiro commit

---
+++ src/main/java/com/example/ecovoltjava/EcoVoltJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecovoltjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoVoltJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoVoltJavaApplication.class, args);
+    }
+
+}
